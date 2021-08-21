7b699d221bb4cf960d5213672a6c1de24429ee61
Lord Ashes <webtable@gmail.com>
2021-08-21 11:14:45 -0400

    Version 1.0.0

---
+++ TaleSpireLightPlugin/Handlers.cs
@@ -0,0 +1,100 @@
+﻿using BepInEx;
+using Bounce.Unmanaged;
+using UnityEngine;
+
+namespace LordAshes
+{
+    public partial class LightPlugin : BaseUnityPlugin
+    {
+        /// <summary>
+        /// Handler for Radial Menu selections
+        /// </summary>
+        /// <param name="cid"></param>
+        /// <param name="lightName"></param>
+        public void RadialMenuRequest(CreatureGuid cid, NGuid rid, string lightName)
+        {
+            Debug.Log("Light Plugin: Requesting A "+lightName+" Light");
+            StatMessaging.SetInfo(new CreatureGuid(rid), LightPlugin.Guid, lightName);
+        }
+
+        /// <summary>
+        /// Handler for Stat Messaging subscribed messages.
+        /// </summary>
+        /// <param name="changes"></param>
+        public void StatMessagingRequest(StatMessaging.Change[] changes)
+        {
+            foreach (StatMessaging.Change change in changes)
+            {
+                Debug.Log("Light Plugin: Processing A " + change.value + " Light Request");
+                if (change.action != StatMessaging.ChangeType.removed)
+                {
+                    ProcessRequest(change.cid, change.value);
+                }
+                else
+                {
+                    ProcessRequest(change.cid, "");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for Radial Menu selections
+        /// </summary>
+        /// <param name="cid"></param>
+        private void ProcessRequest(CreatureGuid cid, string lightName)
+        {
+            CreatureBoardAsset asset;
+            CreaturePresenter.TryGetAsset(cid, out asset);
+
+            if (lightName != "")
+            {
+
+                GameObject socket = GameObject.Find("Effect:Light:" + cid);
+                if (socket == null)
+                {
+                    Debug.Log("Light Plugin: Creating New Light Socket");
+                    socket = new GameObject("Effect:Light:" + cid);
+                    socket.name = "Effect:Light:" + cid;
+                }
+
+                Light light = socket.GetComponent<Light>();
+                if (light == null)
+                {
+                    Debug.Log("Light Plugin: Creating New Light");
+                    light = socket.AddComponent<Light>();
+                    light.name = "Effect:Light:" + cid;
+                }
+
+                Debug.Log("Light Plugin: Adjusting The Light");
+                LightSpecs ls = lights[lightName];
+                string[] c = ls.color.Split(',');
+                string[] p = ls.pos.Split(',');
+                string[] r = ls.rot.Split(',');
+                light.type = ls.lightType;
+                light.color = new UnityEngine.Color(float.Parse(c[0]), float.Parse(c[1]), float.Parse(c[2]));
+                light.intensity = ls.intensity;
+                light.range = ls.range;
+                light.spotAngle = ls.spotAngle;
+                light.shadows = LightShadows.Soft;
+
+                Debug.Log("Light Plugin: Securing Light To Base");
+                socket.transform.position = asset.BaseLoader.transform.position;
+                socket.transform.eulerAngles = asset.BaseLoader.transform.eulerAngles;
+                socket.transform.SetParent(asset.BaseLoader.transform);
+
+                Debug.Log("Light Plugin: Adjusting Light Position");
+                socket.transform.localPosition = new Vector3(float.Parse(p[0]), float.Parse(p[1]), float.Parse(p[2]));
+                socket.transform.localEulerAngles = new Vector3(float.Parse(r[0]), float.Parse(r[1]), float.Parse(r[2]));
+
+            }
+            else
+            {
+                Debug.Log("Light Plugin: Extinguishing Light");
+
+                Debug.Log("Socket Find = " + (GameObject.Find("Effect:Light:" + cid) != null));
+
+                GameObject.Destroy(GameObject.Find("Effect:Light:" + cid));
+            }
+        }
+    }
+}
---
+++ TaleSpireLightPlugin/LightPlugin.cs
@@ -0,0 +1,85 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using Newtonsoft.Json;
+using UnityEngine;
+
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// Notes:
+/// 
+/// 1. Try to keep this main page simple with just the Unity/TS methods like Awake() and Update() methods
+/// 2. Place your processing code in class specific files or a general class file like the sample Handler.cs file
+/// 3. You can make your additional class file part of the main plugin class by making them "public partial class TemplatePlugin : BaseUnityPlugin"
+///    (See the sample Handler.cs and/or Unity.cs files for an example)
+/// 
+/// </summary>
+
+namespace LordAshes
+{
+    [BepInPlugin(Guid, Name, Version)]
+    [BepInDependency(LordAshes.FileAccessPlugin.Guid)]
+    [BepInDependency(LordAshes.StatMessaging.Guid)]
+    [BepInDependency(RadialUI.RadialUIPlugin.Guid)]
+    public partial class LightPlugin : BaseUnityPlugin
+    {
+        // Plugin info
+        public const string Name = "Light Plug-In";                     
+        public const string Guid = "org.lordashes.plugins.light";       
+        public const string Version = "1.0.0.0";                        
+
+        // Configuration
+        private ConfigEntry<KeyboardShortcut> triggerKey { get; set; }
+
+        private Dictionary<string,LightSpecs> lights = new Dictionary<string,LightSpecs>();
+
+        /// <summary>
+        /// Function for initializing plugin
+        /// This function is called once by TaleSpire
+        /// </summary>
+        void Awake()
+        {
+            // Not required but good idea to log this state for troubleshooting purpose
+            UnityEngine.Debug.Log("Light Plugin: Lord Ashes Template Plugin Is Active.");
+
+            // Create root character light menu
+            RadialUI.RadialSubmenu.EnsureMainMenuItem(LightPlugin.Guid, RadialUI.RadialSubmenu.MenuType.character, "Light", FileAccessPlugin.Image.LoadSprite("Light.png"));
+
+            // Create light sub menu
+            RadialUI.RadialSubmenu.CreateSubMenuItem(LightPlugin.Guid, "None", FileAccessPlugin.Image.LoadSprite("light.png"), (cid, s, mmi) => { RadialMenuRequest(cid, RadialUI.RadialUIPlugin.GetLastRadialTargetCreature(), ""); }, true, null);
+            foreach (LightSpecs light in JsonConvert.DeserializeObject<LightSpecs[]>(FileAccessPlugin.File.ReadAllText("LightTypes.json")))
+            {
+                RadialUI.RadialSubmenu.CreateSubMenuItem(LightPlugin.Guid, light.name, FileAccessPlugin.Image.LoadSprite(light.iconName), (cid,s,mmi)=> { RadialMenuRequest(cid, RadialUI.RadialUIPlugin.GetLastRadialTargetCreature(),light.name); }, true, null);
+                lights.Add(light.name, light);
+            }
+
+            // Subscribe to light events
+            StatMessaging.Subscribe(LightPlugin.Guid, StatMessagingRequest);
+
+            // Post on main page
+            Utility.PostOnMainPage(this.GetType());
+        }
+
+        /// <summary>
+        /// Function for determining if view mode has been toggled and, if so, activating or deactivating Character View mode.
+        /// This function is called periodically by TaleSpire.
+        /// </summary>
+        void Update()
+        {
+        }
+
+        public class LightSpecs
+        {
+            public string name { get; set; }
+            public LightType lightType { get; set; }
+            public string iconName { get; set; }
+            public float intensity { get; set; }
+            public string color { get; set; }
+            public float range { get; set; }
+            public string pos { get; set; }
+            public string rot { get; set; }
+            public float spotAngle { get; set; }
+        }
+    }
+}
